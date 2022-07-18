db61b61b71b6ed9dda767b9efc1e0cd86b707cbb
PaoJG13 <109559052+PaoJG13@users.noreply.github.com>
2022-07-18 19:24:49 -0400

    Update board.py

--- board.py
+++ board.py
@@ -2,7 +2,9 @@ firstRow = ["-", "-", "-"];
 secondRow = ["-", "-", "-"];
 thirdRow = ["-", "-", "-"];
 
+print("\n");
 print("Printing board...")
+print("\n");
 print(firstRow);
 print(secondRow);
 print(thirdRow);
