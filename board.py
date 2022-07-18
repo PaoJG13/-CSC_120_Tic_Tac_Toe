3fc95879e5ca0102d957a9ff462a2d955593a17a
PaoJG13 <109559052+PaoJG13@users.noreply.github.com>
2022-07-18 19:58:23 -0400

    Update board.py

--- board.py
+++ board.py
@@ -1,18 +1,18 @@
-firstRow = ["-", "-", "-"];
-secondRow = ["-", "-", "-"];
-thirdRow = ["-", "-", "-"];
+firstRow = ["-", "-", "-"]
+secondRow = ["-", "-", "-"]
+thirdRow = ["-", "-", "-"]
 
 print("\n");
 print("Printing board...\n")
 
-print(firstRow);
-print(secondRow);
-print(thirdRow);
+print(firstRow)
+print(secondRow)
+print(thirdRow)
 
 def player1():
   print("Player 1, make your move\n")
   row = input("Enter row number (0-2): ")
   column = input("Enter column number (0-2): ")
-  print("Player 1 added a mark at location " + row + ", " + column);
+  print("Player 1 added a mark at location " + row + ", " + column)
 
-  player1();
+  player1()
