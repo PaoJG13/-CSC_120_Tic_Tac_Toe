54e1bd1e6c3d38d4b069d441a74802baeb9c93b4
PaoJG13 <109559052+PaoJG13@users.noreply.github.com>
2022-07-18 19:56:53 -0400

    Update board.py

--- board.py
+++ board.py
@@ -8,3 +8,9 @@ print("Printing board...\n")
 print(firstRow);
 print(secondRow);
 print(thirdRow);
+
+def player1():
+  print("Player 1, make your move\n")
+  row = input("Enter row number (0-2): ")
+  column = input("Enter column number (0-2): ")
+  print("Player 1 added a mark at location " + row + ", " + column);
