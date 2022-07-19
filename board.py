0cfbf44420c06381b1fab89ae91be7cf2cbc8c30
Pao Jiménez <paojg13@gmail.com>
2022-07-19 08:34:37 -0400

    checks if valid input

--- board.py
+++ board.py
@@ -6,9 +6,23 @@ class tictactoe:
             ["-", "-", "-"]
             ]
         print("Printing board...")
-        print(board[0])
-        print(board[1])
-        print(board[2])
+        for row in board:
+            for slot in row:
+                print(slot, end="")
+            print()
 
+    def userInput():
+        row = input("Please enter row number (0-2): ")
+        col = input("Please enter column number (0-2): ")
+        if row!=0 and row!=1 and row!=2:
+        if col!=0 and col!=1 and col!=2:
+            print("**** Invalid row or column. Please select row / col between values 0 to 2 ****")
+            continue
 
-tictactoe.printBoard()
+    def checkBoard(userInput):
+        
+        
+
+printBoard()
+while True:
+    
