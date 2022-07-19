127c486ba4a4534f0f2db120a2c756fa1de863ca
Pao Jiménez <paojg13@gmail.com>
2022-07-19 07:56:00 -0400

    board functionality

--- board.py
+++ board.py
@@ -1,19 +1,12 @@
-firstRow = ["-", "-", "-"]
-secondRow = ["-", "-", "-"]
-thirdRow = ["-", "-", "-"]
+class tictactoe:
+def printBoard():
+	board = [
+         ["-", "-", "-"],
+         ["-", "-", "-"],
+         ["-", "-", "-"]
+]
 
-print("\n");
-print("Printing board...\n")
-
-print(firstRow)
-print(secondRow)
-print(thirdRow)
-
-def player1():
-  print("Player 1, make your move\n")
-  row = input("Enter row number (0-2): ")
-  column = input("Enter column number (0-2): ")
-  print("Player 1 added a mark at location " + row + ", " + column)
-  
-
-player1()
+    print("Printing board...)
+    print(board[0])
+    print(board[1])
+    print(board[2])
