b506c4091ea7b5321f81e29a7bd3fc81f95bb7e7
Pao Jiménez <paojg13@gmail.com>
2022-07-19 08:35:57 -0400

    correction on input checking

--- board.py
+++ board.py
@@ -14,8 +14,10 @@ class tictactoe:
     def userInput():
         row = input("Please enter row number (0-2): ")
         col = input("Please enter column number (0-2): ")
-        if row!=0 and row!=1 and row!=2:
-        if col!=0 and col!=1 and col!=2:
+        if (row!=0 and row!=1 and row!=2):
+            print("**** Invalid row or column. Please select row / col between values 0 to 2 ****")
+            continue
+        if (col!=0 and col!=1 and col!=2):
             print("**** Invalid row or column. Please select row / col between values 0 to 2 ****")
             continue
 
